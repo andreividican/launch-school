ea2f7409baf395e2e9e295c27ddfaf5a0a2e0e65
Andrei Vidican <andrei.vidican@tide.co>
2022-09-16 21:47:31 +0100

    Implement internationalization

--- lesson2/calculator.js
+++ lesson2/calculator.js
@@ -1,36 +1,75 @@
 const readLine = require('readline-sync');
+const messages = require('./messages.json');
 
 const prompt = msg => console.log(`>>> ${msg}`);
 
-const invalidNumber = num => num.trimStart() === '' || Number.isNaN(Number(num));
+const invalidNumber = num => {
+  return num.trimStart() === '' || Number.isNaN(Number(num));
+};
 
-prompt('Welcome to the Calculator!');
-prompt('What is the first number?');
+prompt(messages.select);
+let language = readLine.question();
+let selectedLanguage;
+switch (language) {
+  case '1':
+    selectedLanguage = true;
+    break;
+  case '2':
+    selectedLanguage = false;
+    break;
+}
+
+const INTRO = selectedLanguage ? messages.en.intro : messages.ro.intro;
+const QUESTION1 = selectedLanguage ?
+  messages.en.question1 :
+  messages.ro.question1;
+
+const QUESTION2 = selectedLanguage ?
+  messages.en.question2 :
+  messages.ro.question2;
+
+const QUESTION3 = selectedLanguage ?
+  messages.en.question3 :
+  messages.ro.question3;
+
+const INVALID_NUMBER = selectedLanguage ?
+  messages.en.invalidNumber :
+  messages.ro.invalidNumber;
+
+const INVALID_OPERATION = selectedLanguage ?
+  messages.en.invalidNumber :
+  messages.ro.invalidNumber;
+
+const RESULT = selectedLanguage ?
+  messages.en.result :
+  messages.ro.result;
+
+prompt(INTRO);
+prompt(QUESTION1);
 let num1 = readLine.question();
 
 while (invalidNumber(num1)) {
-  prompt('The number you\'ve entered is not valid, please try again.');
+  prompt(INVALID_NUMBER);
   num1 = readLine.question();
 }
 
-prompt('What is the second number?');
+prompt(QUESTION2);
 let num2 = readLine.question();
 
 while (invalidNumber(num2)) {
-  prompt('The number you\'ve entered is not valid, please try again.');
+  prompt(INVALID_NUMBER);
   num2 = readLine.question();
 }
 
-prompt('What operation would you like to perform? \n 1) Add \n 2) Subtract \n 3) Multiply \n 4) Divide');
+prompt(QUESTION3);
 let operation = readLine.question();
 
 while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Must choose 1, 2, 3 or 4, please select again: ');
+  prompt(INVALID_OPERATION);
   operation = readLine.question();
 }
 
 let output;
-const RESULT_MSG = 'The result is: ';
 
 switch (operation) {
   case '1':
@@ -47,5 +86,5 @@ switch (operation) {
     break;
 }
 
-prompt(RESULT_MSG + output);
+prompt(RESULT + output);
 
